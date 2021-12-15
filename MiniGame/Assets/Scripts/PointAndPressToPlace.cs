7749b9adfb3efa15d9025a90754be57f4a6d95ab
BeaniBum <60374872+BeaniBum@users.noreply.github.com>
2021-12-14 19:26:26 -0700

    Minigame done adding touches to finished game
    Added a new object that is the slot added
    added a raycast to the right hand
    added pointandpushtoplace code  to the slot block
    added a new material that is the block hover material
    added code to the slot block that allows the block to detect if the ray hits it
    added simple raycast interaction to the slot
    added code to the jump pad and boost pad that will automatically detect the player

---
+++ MiniGame/Assets/Scripts/PointAndPressToPlace.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointAndPressToPlace : MonoBehaviour
+{
+    public Material neutralMaterial;
+    public Material hoverMaterial;
+    public GameObject Slot;
+    public MeshRenderer Collider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        //Collider = Slot.transform.GetChild(1).gameObject;
+        neutralMaterial = Collider.GetComponent<Renderer>().material;
+        
+    }
+
+
+    public void hoverColor()
+    {
+        Collider.material = hoverMaterial;
+    }
+
+    public void neutralColor()
+    {
+        Collider.material = neutralMaterial;
+    }
+
+    void Update()
+    {
+        
+    }
+}
